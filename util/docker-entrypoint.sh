4d586c0ea6fe62ddb4a1798290fe5f8691a96620
LogCreative <logcreative@outlook.com>
2024-07-17 16:55:27 +0800

    fix: remove the git download again

--- util/docker-entrypoint.sh
+++ util/docker-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# Cleanup www folder
-rm -rf /var/www
-# Copy and install the latest & greatest Latex-Online
-git clone https://github.com/aslushnikov/latex-online /var/www
 cd /var/www
 
 # change to a local server
